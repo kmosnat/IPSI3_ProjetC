a2fbed96c88af1b78745bdec4a89e12d4b919aff
Kyllian MOSNAT <134300224+kmosnat@users.noreply.github.com>
2024-11-20 11:50:44 +0100

    Ajout SDK Smarteck

--- Couleur/Form1.Designer.cs
+++ Couleur/Form1.Designer.cs
@@ -28,18 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            gbCamera = new GroupBox();
+            pictureBox1 = new PictureBox();
+            boutAcquisition = new Button();
+            boutStop = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // gbCamera
+            // 
+            gbCamera.Location = new Point(23, 22);
+            gbCamera.Name = "gbCamera";
+            gbCamera.Size = new Size(532, 283);
+            gbCamera.TabIndex = 0;
+            gbCamera.TabStop = false;
+            gbCamera.Text = "Caméra";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(583, 39);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1193, 873);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // boutAcquisition
+            // 
+            boutAcquisition.Location = new Point(73, 339);
+            boutAcquisition.Name = "boutAcquisition";
+            boutAcquisition.Size = new Size(150, 46);
+            boutAcquisition.TabIndex = 2;
+            boutAcquisition.Text = "Acquisition";
+            boutAcquisition.UseVisualStyleBackColor = true;
+            boutAcquisition.Click += boutAcquisition_Click;
+            // 
+            // boutStop
+            // 
+            boutStop.Location = new Point(323, 339);
+            boutStop.Name = "boutStop";
+            boutStop.Size = new Size(150, 46);
+            boutStop.TabIndex = 3;
+            boutStop.Text = "Arret";
+            boutStop.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1466, 945);
+            ClientSize = new Size(1823, 945);
+            Controls.Add(boutStop);
+            Controls.Add(boutAcquisition);
+            Controls.Add(pictureBox1);
+            Controls.Add(gbCamera);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox gbCamera;
+        private PictureBox pictureBox1;
+        private Button boutAcquisition;
+        private Button boutStop;
     }
 }
--- Couleur/Form1.cs
+++ Couleur/Form1.cs
@@ -1,10 +1,29 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+using System.Windows.Forms;
+using System.Threading;
+
+
 namespace Couleur
 {
     public partial class Form1 : Form
     {
+
+        smcs.IDevice m_device;
+        Rectangle m_rect;
+        PixelFormat m_pixelFormat;
+        UInt32 m_pixelType;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void boutAcquisition_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
