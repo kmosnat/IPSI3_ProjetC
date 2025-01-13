f0c412473fe6f7a72b3b00f732f579be96da817a
Kyllian MOSNAT <134300224+kmosnat@users.noreply.github.com>
2025-01-13 11:42:13 +0100

    add fille for log dll

---
+++ Log/Log.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Log
+{
+    public enum LogSource
+    {
+        Arduino,
+        Client,
+        Serveur,
+        Camera
+    }
+
+    public enum LogLevel
+    {
+        INFO,
+        WARNING,
+        ERROR
+    }
+
+    public class Log
+    {
+        public LogSource Source { get; set; }
+        public LogLevel Level { get; set; }
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+
+        public Log(LogSource source, LogLevel level, string message)
+        {
+            Source = source;
+            Level = level;
+            Message = message;
+            Time = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{Time}:\n[{Source}][{Level}] - {Message}";
+        }
+    }
+}
