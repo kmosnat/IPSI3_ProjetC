66b5d746a4730115e03f7b564b5d223283c1bf27
kyllian <kyllian.mosnat@gmail.com>
2025-01-19 21:24:47 +0100

    add RobotObject and RobotState classes for logging functionality

---
+++ Log/RobotState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Log
+{
+    public enum RobotState
+    {
+        Wait,
+        OnProcess,
+        RobotOnMoving
+    }
+}
